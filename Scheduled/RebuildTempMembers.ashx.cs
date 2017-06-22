2d8e695fb2f986102b79ae26c8073e9d7ed3e09b
Mark <mark@coalface.co.uk>
2017-06-22 11:29:53 +0100

    Amended Cardpresso generation to include multiple group cards

--- Scheduled/RebuildTempMembers.ashx.cs
+++ Scheduled/RebuildTempMembers.ashx.cs
@@ -21,13 +21,15 @@ namespace CRM.Scheduled
             db.ExecuteCommand("DBCC CHECKIDENT('Cardpresso', RESEED, 0);");
             db.SubmitChanges();
 
+            // standard memberships //
+
             var activeMemberships = from p in (from p in db.CRM_AnnualPasses
                                     where p.ExpiryDate >= DateTime.Now.Date
                                     where !p.IsArchived
                                     select p).ToList()
                                     where p.IsCurrent
                                     let CRM_Person = db.CRM_Persons.SingleOrDefault(s => s.Reference == p.PrimaryContactReference)
-                                    where p.CRM_AnnualPassType.IsWebsite
+                                    where p.CRM_AnnualPassType.IsWebsite && p.CRM_AnnualPassType.GroupSize == 0
                                     where CRM_Person != null
                                     select new Cardpresso()
                                     {
@@ -38,6 +40,36 @@ namespace CRM.Scheduled
                                     };
 
             db.Cardpressos.InsertAllOnSubmit(activeMemberships);
+            db.SubmitChanges();
+
+            // group memberships //
+
+            var activeGroupMemberships = from p in (from p in db.CRM_AnnualPasses
+                    where p.ExpiryDate >= DateTime.Now.Date
+                    where !p.IsArchived
+                    select p).ToList()
+                where p.IsCurrent
+                let CRM_Person = db.CRM_Persons.SingleOrDefault(s => s.Reference == p.PrimaryContactReference)
+                where p.CRM_AnnualPassType.IsWebsite && p.CRM_AnnualPassType.GroupSize > 0
+                where CRM_Person != null
+                select p;
+
+            foreach (var activeGroupMembership in activeGroupMemberships)
+            {
+                var person = db.CRM_Persons.SingleOrDefault(s => s.Reference == activeGroupMembership.PrimaryContactReference);
+
+                foreach (var crmAnnualPassCorporate in activeGroupMembership.CRM_AnnualPassCorporates)
+                {
+                    db.Cardpressos.InsertOnSubmit(new Cardpresso()
+                    {
+                        Barcode = activeGroupMembership.CRM_AnnualPassCard.MembershipNumber.ToString(),
+                        Expiry = activeGroupMembership.ExpiryDate,
+                        Membership = person.OrganisationName,
+                        Name = crmAnnualPassCorporate.Name
+                    });
+                }
+            }
+
             db.SubmitChanges();
 
             context.Response.Write("DONE - " + activeMemberships.Count() + " records");
